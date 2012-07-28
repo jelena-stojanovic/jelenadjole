5f9a43abb7cf80b99185c322def74e0eb99f28c3
DjordjeGligorijevic90@gmail.com <DjordjeGligorijevic90@gmail.com@e766457e-1103-e88b-7b7a-81b7a5b221b6>
2012-07-28 13:16:34 +0000

    added CreateDoubleMatrix class third time

--- PS_MSSP_Server/src/logic/SO/Import_Export/CreateDoubleMatrix.java
+++ PS_MSSP_Server/src/logic/SO/Import_Export/CreateDoubleMatrix.java
@@ -11,16 +11,14 @@ import java.util.ArrayList;
  * @author Djordje
  */
 public class CreateDoubleMatrix {
-    
-    public static double[][] create (ArrayList<Double[]> datasetValues) {
+
+    public static double[][] create(ArrayList<Double[]> datasetValues) {
         double[][] doubleMatrix = new double[datasetValues.size()][datasetValues.get(0).length];
         for (int i = 0; i < datasetValues.size(); i++) {
             for (int j = 0; j < datasetValues.get(i).length; j++) {
                 doubleMatrix[i][j] = datasetValues.get(i)[j];
             }
         }
-        System.out.println("have fun");
         return doubleMatrix;
-        
     }
 }
