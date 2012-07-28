0fa75d18c53b0071af107ead898083d3b4e31fc5
jelena.stojanovic.1989@gmail.com <jelena.stojanovic.1989@gmail.com@e766457e-1103-e88b-7b7a-81b7a5b221b6>
2012-07-28 09:20:15 +0000

    DateAttribute

---
+++ trunk/PS_MSSP_Zajednicki/src/model/attribute/DateAttribute.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.attribute;
+
+/**
+ *
+ * @author Jelena
+ */
+public class DateAttribute extends Attribute{
+
+    @Override
+    public boolean isNominal() {
+       return false;
+    }
+
+    @Override
+    public boolean isNumerical() {
+        return false;
+    }
+
+    @Override
+    public boolean isOrdinal() {
+        return false;
+    }
+
+    @Override
+    public boolean isInterval() {
+        return false;
+    }
+
+    @Override
+    public boolean isDate() {
+        return true;
+    }
+
+    @Override
+    public Object getPossibleValues() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
