31163b89b8464323e8c128abd116d90ffbe1369d
jelena.stojanovic.1989@gmail.com <jelena.stojanovic.1989@gmail.com@e766457e-1103-e88b-7b7a-81b7a5b221b6>
2012-07-28 11:10:31 +0000

    Attribute index, role

--- PS_MSSP_Zajednicki/src/model/attribute/Attribute.java
+++ PS_MSSP_Zajednicki/src/model/attribute/Attribute.java
@@ -9,11 +9,11 @@ package model.attribute;
  * @author Jelena
  */
 public abstract class Attribute {
-    
-    String name;
-    String description;
-    int missingValues;
-    Ontology attributeRole;
+    private int indexOfAttribute;
+    private String name;
+    private String description;
+    private int missingValues;
+    private int attributeRole;
     
     public abstract boolean isNominal();
     public abstract boolean isNumerical();
@@ -23,6 +23,76 @@ public abstract class Attribute {
     
    
     public abstract Object getPossibleValues();
+
+    /**
+     * @return the indexOfAttribute
+     */
+    public int getIndexOfAttribute() {
+        return indexOfAttribute;
+    }
+
+    /**
+     * @param indexOfAttribute the indexOfAttribute to set
+     */
+    public void setIndexOfAttribute(int indexOfAttribute) {
+        this.indexOfAttribute = indexOfAttribute;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the missingValues
+     */
+    public int getMissingValues() {
+        return missingValues;
+    }
+
+    /**
+     * @param missingValues the missingValues to set
+     */
+    public void setMissingValues(int missingValues) {
+        this.missingValues = missingValues;
+    }
+
+    /**
+     * @return the attributeRole
+     */
+    public int getAttributeRole() {
+        return attributeRole;
+    }
+
+    /**
+     * @param attributeRole the attributeRole to set
+     */
+    public void setAttributeRole(int attributeRole) {
+        this.attributeRole = attributeRole;
+    }
     
     
    
