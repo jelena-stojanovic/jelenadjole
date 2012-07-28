5d5b57b49b3dbcd7cfccfc01fb94a6070151a96c
DjordjeGligorijevic90@gmail.com <DjordjeGligorijevic90@gmail.com@e766457e-1103-e88b-7b7a-81b7a5b221b6>
2012-07-28 12:27:01 +0000

    nothing

--- PS_MSSP_Zajednicki/src/model/dataset/DataTable.java
+++ PS_MSSP_Zajednicki/src/model/dataset/DataTable.java
@@ -4,10 +4,7 @@
  */
 package model.dataset;
 
-import java.io.IOException;
 import org.ejml.data.DenseMatrix64F;
-import org.ejml.data.Matrix64F;
-import org.ejml.ops.MatrixIO;
 
 /**
  *
@@ -18,33 +15,30 @@ public class DataTable {
     DenseMatrix64F matrix;
 
     public DataTable(double[][] data) {
-        matrix= new DenseMatrix64F(data);
+        matrix = new DenseMatrix64F(data);
     }
 
     public void setMatrix(DenseMatrix64F matrix) {
         this.matrix = matrix;
     }
-     
-     
-     public double getValue(int row, int column){
-         return matrix.get(row, column);
-     }
-      public int getIndex( int row , int col ) {
+
+    public double getValue(int row, int column) {
+        return matrix.get(row, column);
+    }
+
+    public int getIndex(int row, int col) {
         return matrix.getIndex(row, col);
     }
-     
-     public void set(int row, int column, double value){
-         matrix.set(row, column, value);
-     }
-     
-      public void add( int row , int col , double value ) {
-          matrix.add(row, col, value);
-      }
 
-       public int getNumElements() {
-        return matrix.getNumElements();
+    public void set(int row, int column, double value) {
+        matrix.set(row, column, value);
     }
 
-    
-    
+    public void add(int row, int col, double value) {
+        matrix.add(row, col, value);
+    }
+
+    public int getNumElements() {
+        return matrix.getNumElements();
+    }
 }
