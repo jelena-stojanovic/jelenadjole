c6041b63a168ed0b5159e141e5b2d67abe2751da
jelena.stojanovic.1989@gmail.com <jelena.stojanovic.1989@gmail.com@e766457e-1103-e88b-7b7a-81b7a5b221b6>
2012-08-04 13:44:13 +0000

    Import wizard skoro pa zavrsen

--- PS_MSSP_Zajednicki/src/model/attribute/Ontology.java
+++ PS_MSSP_Zajednicki/src/model/attribute/Ontology.java
@@ -4,6 +4,9 @@
  */
 package model.attribute;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Jelena
@@ -12,4 +15,28 @@ public class Ontology {
     public static final int CLASS_ATTRIBUTE=1;
     public static final int REGULAR_ATTRIBUTE=2;
     public static final int ID_ATTRIBUTE=3;
+    
+    public static ArrayList<String> getAttributeRoles(){
+        ArrayList<String> list= new ArrayList<String>();
+        list.add("Attribute");
+        list.add("Class");
+        list.add("Id");
+        return list;
+    }
+    
+    public static ArrayList<String> getAttributeTypes(){
+        ArrayList<String> list= new ArrayList<String>();
+        list.add("Nominal");
+        list.add("Numerical");
+        list.add("Ordinal");
+        list.add("Interval");
+        list.add("Date");
+        return list;
+    }
+    
+    public static final String NUMERICAL_ATTRIBUTE="Numerical";
+    public static final String NOMINAL_ATTRIBUTE="Nominal";
+    public static final String ORDINAL_ATTRIBUTE="Ordinal";
+    public static final String INTERVAL_ATTRIBUTE="Interval";
+    public static final String DATE_ATTRIBUTE="Date";
 }
