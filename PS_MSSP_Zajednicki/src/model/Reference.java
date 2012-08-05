690a07a3d41bf8912e99f591aea9d1d5f54f3b6e
DjordjeGligorijevic90@gmail.com <DjordjeGligorijevic90@gmail.com@e766457e-1103-e88b-7b7a-81b7a5b221b6>
2012-08-05 18:06:44 +0000

    implemented ODO interface

--- PS_MSSP_Zajednicki/src/model/Reference.java
+++ PS_MSSP_Zajednicki/src/model/Reference.java
@@ -4,14 +4,18 @@
  */
 package model;
 
+import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import tools.KonverterTipova;
 
 /**
  *
  * @author Jelena
  */
-public class Reference {
+public class Reference implements OpstiDomenskiObjekat {
+
+    private int referenceID;
     private String author;
     private String title;
     private Date date;
@@ -98,11 +102,125 @@ public class Reference {
     public void setOtherInformation(String otherInformation) {
         this.otherInformation = otherInformation;
     }
+
     @Override
-    public String toString() {
-        return  author + "; " + title + "; " + date + ", " + location + ". " + otherInformation;
+    public String vratiVrednostiAtributa() {
+        java.sql.Date sqlDate = KonverterTipova.Konvertuj(date);
+        return "" + referenceID + ", '" + author + "', '" + title + "', " + sqlDate + ", '" + location + "', '" + otherInformation + "'";
     }
-    
-    
-    
+
+    @Override
+    public String postaviVrednostiAtributa() {
+        java.sql.Date sqlDate = KonverterTipova.Konvertuj(date);
+        return "reference ID = " + referenceID + ",author = '" + author + "', title ='" + title + "', date = "
+                + sqlDate + ", location = '" + location + "', otherInformation = '" + otherInformation + "'";
+    }
+
+    @Override
+    public String vratiImeKlase() {
+        return "Reference";
+    }
+
+    @Override
+    public String vratiUslovZaNadjiSlog() {
+        return " referenceID = '" + referenceID + "'";
+    }
+
+    @Override
+    public String vratiUslovZaNadjiSlogove() {
+        return "";
+    }
+
+    @Override
+    public String vratiAtributPretrazivanja() {
+        return "referenceID";
+    }
+
+    @Override
+    public boolean Napuni(ResultSet RSslog) {
+        try {
+            referenceID = KonverterTipova.Konvertuj(RSslog, referenceID, "referenceID");
+            author = KonverterTipova.Konvertuj(RSslog, author, "author");
+            title = KonverterTipova.Konvertuj(RSslog, title, "title");
+            date = KonverterTipova.Konvertuj(KonverterTipova.Konvertuj(RSslog, "date"));
+            location = KonverterTipova.Konvertuj(RSslog, location, "location");
+            otherInformation = KonverterTipova.Konvertuj(RSslog, otherInformation, "otherInformation");
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int povecajBroj(ResultSet rs) {
+        try {
+            referenceID = KonverterTipova.Konvertuj(rs, referenceID, "referenceID");
+            return ++referenceID;
+        } catch (Exception e) {
+            System.out.println("Izuzetak kod generisanja novog broja racuna: " + e);
+            return 0;
+        }
+    }
+
+    @Override
+    public OpstiDomenskiObjekat vratiVezaniObjekat(int brojVezanogObjekta) {
+        return null;
+    }
+
+    @Override
+    public void Napuni(ResultSet RSslog, int brojSloga, int brojVezanogObjekta) {
+    }
+
+    @Override
+    public void kreirajVezaniObjekat(int brojStavkiVezanogObjekta, int brojVezanogObjekta) {
+    }
+
+    @Override
+    public int vratiBrojVezanihObjekata() {
+        return -1;
+    }
+
+    @Override
+    public void postaviPocetniBroj() {
+        referenceID = 1;
+    }
+
+    @Override
+    public OpstiDomenskiObjekat vratiSlogVezanogObjekta(int brojVezanogObjekta, int brojSloga) {
+        return null;
+    }
+
+    @Override
+    public int vratiBrojSlogovaVezanogObjekta(int brojVezanogObjekta) {
+        return -1;
+    }
+
+    @Override
+    public boolean vrednosnaOgranicenja() {
+        Date newDate = new Date();
+        if (date.before(newDate)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void Obradi() {
+    }
+
+    @Override
+    public void Storniraj() {
+    }
+
+    @Override
+    public String vratiNazivNovogObjekta() {
+        return "new reference";
+    }
+
+    @Override
+    public String vratiNazivObjekta() {
+        return "reference";
+    }
 }
