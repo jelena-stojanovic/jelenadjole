63299fd9b75adb411067de44d5244f76a43125b6
jelena.stojanovic.1989@gmail.com <jelena.stojanovic.1989@gmail.com@e766457e-1103-e88b-7b7a-81b7a5b221b6>
2012-08-06 12:14:57 +0000

    CSVExportToDSFile

--- PS_MSSP_Server/src/logic/SO/Import_Export/CSVExportDSToFile.java
+++ PS_MSSP_Server/src/logic/SO/Import_Export/CSVExportDSToFile.java
@@ -8,12 +8,16 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.attribute.Attribute;
+import model.attribute.DateAttribute;
 import model.attribute.NominalAttribute;
+import model.dataFormat.CSVFormat;
 import model.dataset.DataSet;
 
 /**
@@ -24,15 +28,18 @@ public class CSVExportDSToFile {
 
     List<Attribute> attributes = null;
 
-    public void export(String filePath, DataSet dataset, boolean writeAttributeName, char columnSeparator) {
+    public void export( DataSet dataset,CSVFormat csvFormat ) throws Exception {
+        boolean writeAttributeName= csvFormat.isUseFirstRowAsAttributeNames();
+        char columnSeparator= csvFormat.getColumnSeparator();
+        String filePath=csvFormat.getCsvFile().getPath();
         PrintWriter out = null;
         try {
             out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(filePath)));
             attributes = dataset.getAttributes();
-            List<Integer> nominalAttributeIndexes = getNominalAttributeIndex();
-
+            
+            //*****write name of attribute*******/
             if (writeAttributeName == true) {
-                out.write(attributes.get(0).getName());
+                out.print(attributes.get(0).getName());
 
                 for (int i = 1; i < attributes.size(); i++) {
                     Attribute attribute = attributes.get(i);
@@ -40,29 +47,54 @@ public class CSVExportDSToFile {
                 }
                 out.println();
             }
+            //***** end of write name of attribute*******/
 
-            int noOfColums = dataset.getDataTable().getNumColums();
-            int noOfRows = dataset.getDataTable().getNumRows();
+            //******* write dataMatrix****/
             double[][] doubleMatrix = dataset.getDataTable().getDoubleMatrix();
-            //                               trebalo bi da je broj redova
+
+            //no of rows =i
             for (int i = 0; i < doubleMatrix.length; i++) {
                 double[] ds = doubleMatrix[i];
-                out.print(String.valueOf(ds[0]));
-                //                      broj kolona
+
+                /***first value in row ***/
+                double d1=ds[0];
+                
+                if (attributes.get(0).isNominal()) {
+                        NominalAttribute noma = (NominalAttribute) attributes.get(i);
+                        String value = noma.getNominalValueFromIndex(d1);
+                        out.print(',' +value);
+                    }else if(attributes.get(0).isDate()){
+                        DateAttribute da= (DateAttribute)attributes.get(i);
+                        Date date = da.getDateFromDouble(d1);
+                        String format= csvFormat.getDatePattern();
+                        String dateInFormat= new SimpleDateFormat(format).format(date);
+                        out.print(',' + dateInFormat);
+                    }else {
+                        out.print(',' + String.valueOf(d1));
+                    }
+                /***end of first value in row ***/
+                
+                //no of columns =j
                 for (int j = 1; j < ds.length; j++) {
                     double d = ds[j];
-                    if (nominalAttributeIndexes.contains(j)) {
-                        try {
-                           out.print(MappingValues.getNominalValueFromIndex(((NominalAttribute)attributes.get(j)).getClassToIndexMap(),d));
-                        } catch (Exception ex) {
-                            Logger.getLogger(CSVExportDSToFile.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    } else {
+                    
+                    if (attributes.get(j).isNominal()) {
+                        NominalAttribute noma = (NominalAttribute) attributes.get(i);
+                        String value = noma.getNominalValueFromIndex(d);
+                        out.print(',' +value);
+                    }else if(attributes.get(j).isDate()){
+                        DateAttribute da= (DateAttribute)attributes.get(i);
+                        Date date = da.getDateFromDouble(d);
+                        String format= da.getDatePatern();
+                        String dateInFormat= new SimpleDateFormat(format).format(date);
+                        out.print(',' + dateInFormat);
+                    }else {
                         out.print(',' + String.valueOf(d));
                     }
                 }
                 out.println();
             }
+            //*******end write dataMatrix****/
         } catch (FileNotFoundException ex) {
             Logger.getLogger(CSVExportDSToFile.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
@@ -72,15 +104,5 @@ public class CSVExportDSToFile {
 
     }
 
-    private List<Integer> getNominalAttributeIndex() {
-        List<Integer> indexes = new ArrayList<Integer>();
-        for (int i = 0; i < attributes.size(); i++) {
-            Attribute attribute = attributes.get(i);
-            if (attribute.isNominal()) {
-                indexes.add(i);
-            }
-        }
-
-        return indexes;
-    }
+    
 }
