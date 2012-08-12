e37315bab160f63f6da6e45efd878e8620324ca3
jelena.stojanovic.1989@gmail.com <jelena.stojanovic.1989@gmail.com@e766457e-1103-e88b-7b7a-81b7a5b221b6>
2012-08-12 08:53:42 +0000

    kontroler ki racun

--- PS_MSSP_Klijent/src/view/guicontrollers/KontrolerKIRacun.java
+++ PS_MSSP_Klijent/src/view/guicontrollers/KontrolerKIRacun.java
@@ -5,7 +5,7 @@ package view.guicontrollers;
  *
  * 02.05.2011
  *
- * @autor  Dr Sinisa Vlajic
+ * @autor Dr Sinisa Vlajic
  *
  * Katedra za softversko inzenjerstvo
  *
@@ -14,46 +14,45 @@ package view.guicontrollers;
  * Fakultet organizacionih nauka - Beograd
  *
  */
-
 import java.io.*;
 import view.forms.EkranskaFormaRacun;
 
+public class KontrolerKIRacun extends OpstiKontrolerKI {
 
-public class KontrolerKIRacun extends OpstiKontrolerKI
-{
-  KontrolerKIRacun(EkranskaFormaRacun efr) throws IOException {oef = efr;}
+    KontrolerKIRacun(EkranskaFormaRacun efr) throws IOException {
+        oef = efr;
+    }
 
+    public void KonvertujGrafickiObjekatUDomenskiObjekat() {
+        Racun rac = (Racun) odo;
+        EkranskaFormaRacun efr = (EkranskaFormaRacun) oef;
+        rac.BrojRacuna = KonverterTipova.Konvertuj(efr.BrojRacuna, rac.BrojRacuna);
+        rac.NazivPartnera = KonverterTipova.Konvertuj(efr.NazivPartnera, rac.NazivPartnera);
+        rac.UkupnaVrednost = KonverterTipova.Konvertuj(efr.UkupnaVrednost, rac.UkupnaVrednost);
+        rac.Obradjen = KonverterTipova.Konvertuj(efr.Obradjen, rac.Obradjen);
+        rac.Storniran = KonverterTipova.Konvertuj(efr.Storniran, rac.Storniran);
+        if (efr.TabStavkaRacuna.getRowCount() > 0) {
+            StavkaRacuna[] nizObjekata = new StavkaRacuna[efr.TabStavkaRacuna.getRowCount()];
+            for (int i = 0; i < efr.TabStavkaRacuna.getRowCount(); i++) {
+                nizObjekata[i] = new StavkaRacuna(rac);
+            }
+            rac.sracun = nizObjekata;
+            KonverterTipova.KonvertujTabeluUNizObjekata(efr.TabStavkaRacuna, nizObjekata);
+        } else {
+            rac.sracun = null;
+        }
+    }
 
-public void KonvertujGrafickiObjekatUDomenskiObjekat()
-   {
-	  Racun rac = (Racun) odo;
-      EkranskaFormaRacun efr = (EkranskaFormaRacun) oef;
-	  rac.BrojRacuna = KonverterTipova.Konvertuj(efr.BrojRacuna,rac.BrojRacuna);
-      rac.NazivPartnera = KonverterTipova.Konvertuj(efr.NazivPartnera,rac.NazivPartnera);
-      rac.UkupnaVrednost = KonverterTipova.Konvertuj(efr.UkupnaVrednost,rac.UkupnaVrednost);
-      rac.Obradjen = KonverterTipova.Konvertuj(efr.Obradjen,rac.Obradjen);
-      rac.Storniran = KonverterTipova.Konvertuj(efr.Storniran,rac.Storniran);
-      if (efr.TabStavkaRacuna.getRowCount() > 0)
-       {  StavkaRacuna [] nizObjekata = new StavkaRacuna[efr.TabStavkaRacuna.getRowCount()];
-          for(int i = 0; i< efr.TabStavkaRacuna.getRowCount(); i++)
-		      nizObjekata[i] = new StavkaRacuna(rac);
-		  rac.sracun = nizObjekata;
-          KonverterTipova.KonvertujTabeluUNizObjekata(efr.TabStavkaRacuna,nizObjekata);
-       }
-      else
-          rac.sracun = null;
-   }
-
-
- public void KonvertujDomenskiObjekatUGrafickiObjekat()
-   {  Racun rac = (Racun) odo;
-      EkranskaFormaRacun efr = (EkranskaFormaRacun) oef;
-	  KonverterTipova.Konvertuj(rac.BrojRacuna,efr.BrojRacuna);
-      KonverterTipova.Konvertuj(rac.NazivPartnera,efr.NazivPartnera);
-      KonverterTipova.Konvertuj(rac.UkupnaVrednost,efr.UkupnaVrednost);
-      KonverterTipova.Konvertuj(rac.Obradjen,efr.Obradjen);
-      KonverterTipova.Konvertuj(rac.Storniran,efr.Storniran);
-      if (rac.sracun != null)
-      	KonverterTipova.KonvertujNizObjekataUTabelu(rac.sracun,efr.TabStavkaRacuna);
-   }
+    public void KonvertujDomenskiObjekatUGrafickiObjekat() {
+        Racun rac = (Racun) odo;
+        EkranskaFormaRacun efr = (EkranskaFormaRacun) oef;
+        KonverterTipova.Konvertuj(rac.BrojRacuna, efr.BrojRacuna);
+        KonverterTipova.Konvertuj(rac.NazivPartnera, efr.NazivPartnera);
+        KonverterTipova.Konvertuj(rac.UkupnaVrednost, efr.UkupnaVrednost);
+        KonverterTipova.Konvertuj(rac.Obradjen, efr.Obradjen);
+        KonverterTipova.Konvertuj(rac.Storniran, efr.Storniran);
+        if (rac.sracun != null) {
+            KonverterTipova.KonvertujNizObjekataUTabelu(rac.sracun, efr.TabStavkaRacuna);
+        }
+    }
 }
