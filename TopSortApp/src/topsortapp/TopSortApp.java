c0e79aa95f7f7c30c71dd10207e2f64771306831
jelena.stojanovic.1989@gmail.com <jelena.stojanovic.1989@gmail.com@e766457e-1103-e88b-7b7a-81b7a5b221b6>
2013-08-30 13:13:55 +0000

    Jelenin domaci

---
+++ TopSortApp/src/topsortapp/TopSortApp.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package topsortapp;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jelena
+ */
+public class TopSortApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        // TODO code application logic here
+    }
+    
+    
+    public int[] readArray(String fileName){
+        
+        //read n--- first row
+        
+        //initialize pred array
+        //initialize succ array
+        
+        ArrayList al=new ArrayList<>();
+        
+        
+        //read m pairs, do 00
+        
+        
+        
+        // put it in a list
+        
+        return new int[5];
+    }
+    
+}
