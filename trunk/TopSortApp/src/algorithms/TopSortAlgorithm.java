a4d0a936014d75f0d39a0c727aa54fcc536d7b5b
jelena.stojanovic.1989@gmail.com <jelena.stojanovic.1989@gmail.com@e766457e-1103-e88b-7b7a-81b7a5b221b6>
2013-08-30 13:13:55 +0000

    Jelenin domaci

---
+++ trunk/TopSortApp/src/algorithms/TopSortAlgorithm.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms;
+
+/**
+ *
+ * @author jelena
+ */
+public class TopSortAlgorithm {
+
+    
+    
+    
+    
+    public void topsorts() {
+//   if (bag is not empty){
+//
+//      while (there is an object not taken out yet){
+//
+//         Take it out of the Bag, put it in the output array,
+//         traverse its succ list, reduce the pred count for 
+//         each successor, and if it goes to zero, put it in 
+//         the Bag
+//         topsorts();
+//         Reverse the above
+//
+//      }
+//   } 
+//   else{
+//      Output the output array
+//   }
+    }
+}
