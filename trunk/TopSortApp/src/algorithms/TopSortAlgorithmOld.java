6cc87722e57b98669fe8876bfb940c862daba49a
jelena.stojanovic.1989@gmail.com <jelena.stojanovic.1989@gmail.com@e766457e-1103-e88b-7b7a-81b7a5b221b6>
2013-09-09 03:08:39 +0000

    final

---
+++ trunk/TopSortApp/src/algorithms/TopSortAlgorithmOld.java
@@ -0,0 +1,283 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jelena
+ */
+public class TopSortAlgorithmOld {
+
+    int n = 0;
+    int[] predArray;
+    ArrayList[] succArray;
+    int[] bag;
+    int endPossition = -1;
+    int startPOsition = -1;
+    int[] outputRanking;
+    int numRanked = -1;
+    String filePath;
+
+    public TopSortAlgorithmOld() {
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void readInputArray(String filePath) throws FileNotFoundException, IOException {
+        this.filePath = filePath;
+        if (filePath == null) {
+            System.err.println("Insert correct path");
+        }
+        String line;
+        BufferedReader bufRdr = new BufferedReader(new FileReader(new File(filePath)));
+        boolean first = true;
+        //read n
+        n = Integer.parseInt(bufRdr.readLine());
+        //initialize pred array
+        predArray = new int[n];
+        //initialize succ array
+        succArray = new ArrayList[n];
+        for (int i = 0; i < succArray.length; i++) {
+            succArray[i] = new ArrayList();
+        }
+        bag = new int[n];
+        outputRanking = new int[n];
+
+        System.out.println("Size of array: " + n);
+        int firstInPair = -1;
+        int seconInPair = -1;
+
+        while ((line = bufRdr.readLine()) != null) {
+            if (!line.equals("")) {
+                String[] split = line.split(" ");
+                int size = split.length;
+                //System.out.println("ima ih u redu " + size);
+                if (size == 1) {
+                    int element = Integer.parseInt(split[0]);
+
+                    if (first) {
+                        firstInPair = element;
+                        first = false;
+                        if (firstInPair == 0) {
+                            break;
+                        }
+                    } else {
+                        seconInPair = element;
+                        first = true;
+                        predArray[--seconInPair]++; //since array starts from 0, it is drugi--; increase number of pred for second element in that pair
+                        succArray[--firstInPair].add(--seconInPair);// add second element of that pair in the successors list of the first element
+                    }
+                } else if (size == 2) {
+                    firstInPair = Integer.parseInt(split[0]);
+                    seconInPair = Integer.parseInt(split[1]);
+
+                    if (firstInPair == 0) {
+                        break;
+                    } else {
+                        predArray[--seconInPair]++; //since array starts from 0, it is drugi--; increase number of pred for second element in that pair
+                        succArray[--firstInPair].add(seconInPair);// add second element of that pair in the successors list of the first element
+                    }
+                }
+            }
+        }
+
+        System.out.println("pred " + predArray);
+        System.out.println("succ " + succArray);
+
+        for (int i = 0; i < predArray.length; i++) {
+            int j = predArray[i];
+            System.out.println("Number of predd " + (i + 1) + " is: " + j);
+
+        }
+        System.out.println("*Successors*");
+        for (int i = 0; i < succArray.length; i++) {
+            ArrayList j = succArray[i];
+            System.out.println("Successors of " + (i + 1) + " :");
+            if (j.isEmpty()) {
+                System.out.println("Nema nikog");
+            } else {
+                for (int k = 0; k < j.size(); k++) {
+                    int object = (int) j.get(k);
+                    System.out.print(object + 1 + " ");
+                }
+                System.out.println("");
+            }
+        }
+        //read n--- first row
+        //initialize pred array
+        //initialize succ array
+        //read m pairs, do 00
+        // put it in a list
+    }
+
+    public void bagInitialization() {
+        for (int i = 0; i < predArray.length; i++) {
+            int j = predArray[i];
+            if (j == 0) {
+                bag[++endPossition] = i;
+            }
+        }
+        startPOsition = 0;
+        System.out.println("end position " + endPossition);
+        System.out.println("start position " + startPOsition);
+        System.out.println("Items in bag:");
+        for (int i = startPOsition; i < endPossition + 1; i++) {
+            int j = bag[i] + 1;
+            System.out.println(j);
+        }
+    }
+
+    public boolean bagIsEmpty() {
+        return ((startPOsition > endPossition) || (startPOsition == -1 && endPossition == -1));
+    }
+
+    
+
+    boolean negative = false;
+
+    public void topsorts() {
+        System.out.println("START NEW TOPSORT");
+        //   if (bag is not empty){
+        if (!bagIsEmpty()) {
+//      while (there is an object not taken out yet){
+            //while (startPOsition <= endPossition) {
+            for (int currentStartPosition = startPOsition; currentStartPosition <= endPossition; currentStartPosition++) {
+                int currentEndPosition;
+                System.out.println("STARTNA POZICIJA JE " + startPOsition);
+                System.out.println("TRENUTNA STARTNA POZICIJA JE:" + currentStartPosition);
+                //Take it out of the Bag,
+                int element = bag[currentStartPosition];
+                System.out.println("ELEMENT JE SADA " + (element + 1));
+
+                bag[currentStartPosition] = bag[startPOsition];
+                System.out.println("A bag od iter je posle " + (bag[currentStartPosition] + 1));
+                startPOsition++;
+                currentEndPosition = endPossition;
+                System.out.println("TRENUTNA KRAJNJA POZICIJA JE " + currentEndPosition);
+                outputRanking[++numRanked] = element + 1;//+1 because arrays starts from 0
+//                //put it in the output array,               
+                System.out.println("num ranked " + numRanked);
+                //traverse its succ list, 
+                System.out.println("element is:" + (element + 1));
+                ArrayList succelem = succArray[element];
+                for (int i = 0; i < succelem.size(); i++) {
+                    int ithsucc = Integer.parseInt(String.valueOf(succelem.get(i)));
+                    System.out.println((ithsucc + 1) + " je " + (i + 1) + ". " + " succ of " + (element + 1));
+                    //reduce the pred count for each successor, 
+                    //   System.out.println("pre " + predArray[ithsucc]);
+                    predArray[ithsucc]--;
+                    System.out.println("Sad je njegov broj prethodnika " + predArray[ithsucc]);
+                    //   System.out.println("posle " + predArray[ithsucc]);
+                    //and if it goes to zero, put it in the Bag
+                    if (predArray[ithsucc] == 0) {
+                        System.out.println("dodajem ga u bag");
+                        bag[++endPossition] = ithsucc;//******************zamena starog
+                    } else if (predArray[ithsucc] < 0) {
+                        negative = true;
+                    }
+                }
+                System.out.println("pocetna pozicija:" + startPOsition);
+                System.out.println("krajnja pozicija" + endPossition);
+                System.out.println("Items in bag:");
+                for (int i = startPOsition; i <= endPossition; i++) {
+                    int j = bag[i] + 1;
+                    System.out.println(j);
+                }
+                if (!negative) {
+                    topsorts();
+                }
+                System.out.println("REVERSEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+//         Reverse the above
+//         and if it goes to zero, put it in the Bag
+                System.out.println("PRE-Element after call of topsort is: " + (element + 1));
+                System.out.println("PRE- reversa startna pozicija je " + startPOsition);
+                System.out.println("PRE- reversa end pozicija je : " + endPossition);
+                System.out.println("PRE-A trenutna je: " + currentEndPosition);
+                System.out.println("PRE-Trenutna startna pozicija je , takozvni iter:" + currentStartPosition);
+                //         "remove" it from the output array,
+                //         Take it back in the Bag,
+                startPOsition--;
+                numRanked--;
+                System.out.println("OUTPUT POZICIJA JE SADA " + numRanked);
+                endPossition = currentEndPosition;
+                bag[currentStartPosition] = element;         //******************zamena starog
+                System.out.println("Bag of itera " + currentStartPosition + " je sada" + (element + 1));
+                System.out.println("POSLE-Element je u vrecici na " + startPOsition + " poziciji--- new start");
+                System.out.println("POSLE-A krajnja pozicija je " + endPossition + " stvarni");
+                System.out.println("POSLE-Trenutni end je: " + currentEndPosition);
+                ArrayList listSucc = succArray[element];
+                if (listSucc != null && !listSucc.isEmpty()) {
+                    //traverse its succ list, 
+                    for (int i = 0; i < listSucc.size(); i++) {
+                        //increase the pred count for each successor, 
+                        int ithsuccessor = (int) listSucc.get(i);
+                        predArray[ithsuccessor]++;
+                    }
+                }
+            }
+        } else {
+            //Output the output array
+            if (numRanked == (n - 1)) {
+                counter++;
+                if (counter <= 50) {
+                    outputOutput(outputRanking);
+                }
+            } else {
+                System.err.println("It is not partial order.");
+            }
+        }
+
+    }
+    int counter = 0;
+
+    public void outputOutput(int[] output) {
+        try {
+            PrintWriter out = null;
+            out = new PrintWriter(new BufferedWriter(new FileWriter((filePath + "output"), true)));
+            out.print("Topsort " + counter + " : ");
+            for (int i = 0; i < output.length; i++) {
+                int j = output[i];
+                out.print(j);
+                out.print(" ");
+            }
+            out.println();
+            out.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(TopSortAlgorithmOld.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void printNumberOfPossibleTopsorts() {
+        try {
+            PrintWriter out = null;
+
+            out = new PrintWriter(new BufferedWriter(new FileWriter((filePath + "output"), true)));
+            out.print("Number of possible sorts is: " + counter + ".");
+            out.println();
+            out.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(TopSortAlgorithmOld.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
--- trunk/TopSortApp/src/datastructures/SuccArray.java
+++ trunk/TopSortApp/src/datastructures/SuccArray.java
@@ -10,5 +10,94 @@ package datastructures;
  * @author jelena
  */
 public class SuccArray {
+
+    SuccArrayElement first;
+
+    public SuccArray() {
+    }
+    public SuccArray(int value) {
+        first = new SuccArrayElement(value, null, null);
+    }
+    public SuccArray(SuccArrayElement first) {
+        this.first = first;
+    }
+
+    public void addFirst(int value) {
+        SuccArrayElement currrentFirst=first;
+        first=new SuccArrayElement(value, null, currrentFirst);
+        if(currrentFirst!=null){
+            currrentFirst.setPrevious(first);
+        }
+    }
+
+    public SuccArrayElement getFirst() {
+        return first;
+    }
     
+
+
+    
+    public class SuccArrayElement {
+
+        private SuccArrayElement previous;
+        private SuccArrayElement next;
+        private int value;
+
+        public SuccArrayElement() {
+        }
+
+        public SuccArrayElement(int value){
+            this.value=value;
+        }
+                
+        public SuccArrayElement(int value, SuccArrayElement previous, SuccArrayElement next) {
+            this.value = value;
+            this.previous = previous;
+            this.next = next;
+        }
+
+        /**
+         * @return the previous
+         */
+        public SuccArrayElement getPrevious() {
+            return previous;
+        }
+
+        /**
+         * @param previous the previous to set
+         */
+        public void setPrevious(SuccArrayElement previous) {
+            this.previous = previous;
+        }
+
+        /**
+         * @return the next
+         */
+        public SuccArrayElement getNext() {
+            return next;
+        }
+
+        /**
+         * @param next the next to set
+         */
+        public void setNext(SuccArrayElement next) {
+            this.next = next;
+        }
+
+        /**
+         * @return the value
+         */
+        public int getValue() {
+            return value;
+        }
+
+        /**
+         * @param value the value to set
+         */
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+    }
+
 }
